0cb34420b2d521e1016635873fb3ca24a402861c
JosephOlabode <josepholabode17@gmail.com>
2019-04-29 12:25:59 -0700

    contact-us component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,10 @@ import {PagesRoutingModule} from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import {CommonModule} from '@angular/common';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
-    declarations: [PagesComponent, HomeComponent, AboutComponent],
+    declarations: [PagesComponent, HomeComponent, AboutComponent, ContactUsComponent],
     imports: [
         CommonModule,
         PagesRoutingModule,
