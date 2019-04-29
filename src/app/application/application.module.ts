368dc04cc705208e78ce7e4d5fa653dcf5d29f2e
skemuel007 <skemuel007@gmail.com>
2019-04-29 09:15:29 +0100

    Created Application Component

--- src/app/application/application.module.ts
+++ src/app/application/application.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SharedModule} from '../_shared/modules/shared.module';
 import {AppMaterialModule} from '../_shared/modules/app-material.module';
+import { ApplicationComponent } from './application.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [ApplicationComponent],
     imports: [
         CommonModule,
         SharedModule,
