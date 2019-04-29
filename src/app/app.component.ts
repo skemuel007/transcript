38c457c4ec99f57a43fb9d6b8535c9e58338fde7
skemuel007 <skemuel007@gmail.com>
2019-04-29 15:10:51 +0100

    Updated app component typescript to enable route loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouteConfigLoadEnd, RouteConfigLoadStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'transcript';
+export class AppComponent implements OnInit {
+
+  loadingRouteConfig: boolean;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe( event => {
+      if ( event instanceof RouteConfigLoadStart) {
+         // set loading to true
+        this.loadingRouteConfig = true;
+      } else if ( event instanceof RouteConfigLoadEnd ) {
+        this.loadingRouteConfig = false;
+      }
+    });
+  }
 }
