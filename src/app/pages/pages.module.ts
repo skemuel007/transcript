04a5e1d1620b5382d42dac71e9e5afe9f8bb6548
JosephOlabode <josepholabode17@gmail.com>
2019-04-29 14:36:19 -0700

    Navigation bar, side-bar, footer

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,9 +8,12 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ProceduresComponent } from './procedures/procedures.component';
 import { DeliveryComponent } from './delivery/delivery.component';
 import { FaqComponent } from './faq/faq.component';
+import { NavBarComponent } from './_shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './_shared/footer/footer.component';
+import { SideBarComponent } from './_shared/side-bar/side-bar.component';
 
 @NgModule({
-    declarations: [PagesComponent, HomeComponent, AboutComponent, ContactUsComponent, ProceduresComponent, DeliveryComponent, FaqComponent],
+    declarations: [PagesComponent, HomeComponent, AboutComponent, ContactUsComponent, ProceduresComponent, DeliveryComponent, FaqComponent, NavBarComponent, FooterComponent, SideBarComponent],
     imports: [
         CommonModule,
         PagesRoutingModule,
