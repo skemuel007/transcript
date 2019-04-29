bc48f88390a3a24a5317e5a5eee18a3d92d33120
JosephOlabode <josepholabode17@gmail.com>
2019-04-29 12:29:52 -0700

    Added pages to repo

---
+++ src/app/pages/delivery/delivery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery',
+  templateUrl: './delivery.component.html',
+  styleUrls: ['./delivery.component.sass']
+})
+export class DeliveryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,9 +5,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import {CommonModule} from '@angular/common';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ProceduresComponent } from './procedures/procedures.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
-    declarations: [PagesComponent, HomeComponent, AboutComponent, ContactUsComponent],
+    declarations: [PagesComponent, HomeComponent, AboutComponent, ContactUsComponent, ProceduresComponent, DeliveryComponent, FaqComponent],
     imports: [
         CommonModule,
         PagesRoutingModule,
