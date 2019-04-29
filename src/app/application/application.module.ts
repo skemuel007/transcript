54c051917a87b7e0e521f3f935e6c79191ce7013
skemuel007 <skemuel007@gmail.com>
2019-04-29 09:19:01 +0100

    Updated Application Module and Application Component

--- src/app/application/application.module.ts
+++ src/app/application/application.module.ts
@@ -3,10 +3,12 @@ import {CommonModule} from '@angular/common';
 import {SharedModule} from '../_shared/modules/shared.module';
 import {AppMaterialModule} from '../_shared/modules/app-material.module';
 import { ApplicationComponent } from './application.component';
+import {ApplicationRoutingModule} from './application-routing.module';
 
 @NgModule({
     declarations: [ApplicationComponent],
     imports: [
+        ApplicationRoutingModule,
         CommonModule,
         SharedModule,
         AppMaterialModule
