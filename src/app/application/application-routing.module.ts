4595f5c2a05a66fdb058c027907f75256598d575
skemuel007 <skemuel007@gmail.com>
2019-04-29 09:39:09 +0100

    Updated Application Routing Module and Application Module

--- src/app/application/application-routing.module.ts
+++ src/app/application/application-routing.module.ts
@@ -1,7 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes} from '@angular/router';
+import {ApplicationComponent} from './application.component';
 
 const routes: Routes = [
+    {
+        path: '',
+        component: ApplicationComponent,
+        children: [
+
+        ]
+    }
 
 ];
 
--- src/app/application/application.module.ts
+++ src/app/application/application.module.ts
@@ -4,9 +4,13 @@ import {SharedModule} from '../_shared/modules/shared.module';
 import {AppMaterialModule} from '../_shared/modules/app-material.module';
 import { ApplicationComponent } from './application.component';
 import {ApplicationRoutingModule} from './application-routing.module';
+import { NavbarComponent } from './_shared/navbar/navbar.component';
+import { SidebarComponent } from './_shared/sidebar/sidebar.component';
+import { FooterComponent } from './_shared/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-    declarations: [ApplicationComponent],
+    declarations: [ApplicationComponent, NavbarComponent, SidebarComponent, FooterComponent, DashboardComponent],
     imports: [
         ApplicationRoutingModule,
         CommonModule,
