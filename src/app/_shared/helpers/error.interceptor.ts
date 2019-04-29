78478b0dabd2de91cea4c86e2e85f94ea963ee0f
skemuel007 <skemuel007@gmail.com>
2019-04-29 13:08:51 +0100

    Created and Updated HttpErrorInterceptor file

---
+++ src/app/_shared/helpers/error.interceptor.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {AuthenticationService} from '../services/authentication.service';
+import {ToastrService} from 'ngx-toastr';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(private authenticationService: AuthenticationService,
+                private toastr: ToastrService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        return next.handle(req).pipe(
+            catchError(
+                err => {
+                    if ([401, 403, 408, 500].indexOf(err.status) !== -1) {
+                        if ( err.status === 408) {
+                            this.toastr.error('Request timeout due to poor connnection, logging out');
+                        }
+
+                        this.authenticationService.logout();
+                        location.reload(true);
+                    }
+
+                    const error = err.error.message || err.statusText;
+                    return throwError(error);
+                }
+            )
+        );
+    }
+}
