0adb24f3abdeebef85b954d5b65c1a4ea131c807
skemuel007 <skemuel007@gmail.com>
2019-05-15 15:59:32 +0100

    Updated for user name -> Navbar

--- src/app/application/_shared/navbar/navbar.component.ts
+++ src/app/application/_shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../../../_shared/services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  private userName: string;
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit() {
+    this.userName = this.auth.currentUserValue.name;
   }
 
 }
