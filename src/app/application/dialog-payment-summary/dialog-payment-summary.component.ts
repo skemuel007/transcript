8e7eb7bf9c2f0673ebf65273515481efa71b1bb1
skemuel007 <skemuel007@gmail.com>
2019-05-16 13:11:54 +0100

    Created payment dialog

---
+++ src/app/application/dialog-payment-summary/dialog-payment-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-payment-summary',
+  templateUrl: './dialog-payment-summary.component.html',
+  styleUrls: ['./dialog-payment-summary.component.sass']
+})
+export class DialogPaymentSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
