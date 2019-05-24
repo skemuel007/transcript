0a302bb981b9534ec7112746646a61f4e92eaf39
skemuel007 <skemuel007@gmail.com>
2019-05-24 07:52:14 +0100

    Working on Registry and Busary Modules

--- src/app/admin/busary/busary.module.ts
+++ src/app/admin/busary/busary.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from '@angular/core';
 import { BusaryComponent } from './busary.component';
+import { FooterComponent } from './_shared/footer/footer.component';
+import { HeaderComponent } from './_shared/header/header.component';
 
 @NgModule({
-    declarations: [BusaryComponent],
+    declarations: [BusaryComponent, FooterComponent, HeaderComponent],
     imports: [
 
     ],
--- src/app/admin/registry/registry.module.ts
+++ src/app/admin/registry/registry.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from '@angular/core';
 import { RegistryComponent } from './registry.component';
+import { HeaderComponent } from './_shared/header/header.component';
+import { FooterComponent } from './_shared/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-    declarations: [RegistryComponent],
+    declarations: [RegistryComponent, HeaderComponent, FooterComponent, DashboardComponent],
     imports: [
 
     ],
